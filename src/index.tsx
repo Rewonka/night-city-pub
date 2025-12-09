75b9761e58191a9c98fc77d6dd3f89586cfd761c
Rewonka <rewonka@gmail.com>
2025-12-09 15:18:40 +0100

    Skeleton added

---
+++ src/index.tsx
@@ -0,0 +1,106 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { Canvas, useFrame } from "@react-three/fiber";
+import { Environment, Gltf, PerspectiveCamera } from "@react-three/drei";
+import { XR, createXRStore } from "@react-three/xr";
+
+import { Bullets } from "./bullets";
+import { Gun } from "./gun";
+import ReactDOM from "react-dom/client";
+import { Score } from "./score";
+import { Target } from "./targets";
+import gsap from "gsap";
+
+const xrStore = createXRStore({
+  emulate: {
+    controller: {
+      left: {
+        position: [-0.15649, 1.43474, -0.38368],
+        quaternion: [
+          0.14766305685043335, -0.02471366710960865, -0.0037767395842820406,
+          0.9887216687202454,
+        ],
+      },
+      right: {
+        position: [0.15649, 1.43474, -0.38368],
+        quaternion: [
+          0.14766305685043335, 0.02471366710960865, -0.0037767395842820406,
+          0.9887216687202454,
+        ],
+      },
+    },
+  },
+});
+
+const GsapTicker = () => {
+  useFrame(() => {
+    gsap.ticker.tick();
+  });
+  return null;
+};
+
+const App = () => {
+  return (
+    <>
+      <Canvas
+        style={{
+          position: "fixed",
+          width: "100vw",
+          height: "100vh",
+        }}
+      >
+        <color args={[0x808080]} attach={"background"}></color>
+        <PerspectiveCamera makeDefault position={[0, 1.6, 2]} fov={75} />
+        <Environment preset="warehouse" />
+        <GsapTicker />
+        <XR store={xrStore}></XR>
+      </Canvas>
+      <div
+        style={{
+          position: "fixed",
+          display: "flex",
+          width: "100vw",
+          height: "100vh",
+          flexDirection: "column",
+          justifyContent: "space-between",
+          alignItems: "center",
+          color: "white",
+        }}
+      >
+        <div>
+          <div style={{ paddingTop: "10px" }}>
+            WebXR Night City Pub -&nbsp;
+            <a href="https://github.com/Rewonka/night-city-pub">
+              GitHub
+            </a>
+            &nbsp;|&nbsp; used source Meta webx-first-steps-react &nbsp;
+            <a href="https://github.com/meta-quest/webxr-first-steps-react">
+              GitHub
+            </a>
+          </div>
+        </div>
+        <button
+          onClick={() => xrStore.enterVR()}
+          style={{
+            position: "fixed",
+            bottom: "20px",
+            left: "50%",
+            transform: "translateX(-50%)",
+            fontSize: "20px",
+          }}
+        >
+          Enter VR
+        </button>
+      </div>
+    </>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <App />,
+);
